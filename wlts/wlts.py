116ee635f4f82293c8dfdf856209235cb4ef9cb2
gqueiroz <gribeiro.mg@gmail.com>
2019-11-19 10:44:25 -0300

    adding wlts client class skeleton

---
+++ wlts/wlts.py
@@ -0,0 +1,18 @@
+class wlts:
+    def __init__(self):
+        pass
+
+    def list_collections(self):
+        pass
+
+    def trajectory(self):
+        pass
+
+    def describe_collection(self):
+        pass
+
+    def list_classification_sytem(self):
+        pass
+
+    def describe_classification_sytem(self):
+        pass
