cc7254e54b7e51abbb3370a66dcf82303933116f
gqueiroz <gribeiro.mg@gmail.com>
2019-11-19 16:36:03 -0300

    adding WLTS client wrapper class

--- wlts/wlts.py
+++ wlts/wlts.py
@@ -1,4 +1,18 @@
+#
+# This file is part of Web Land Trajectory Service.
+# Copyright (C) 2019 INPE.
+#
+# Web Land Trajectory Service is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+"""Python API client wrapper for WLTS."""
+
 class wlts:
+    """This class implements a Python API client wrapper for WLTS.
+
+     See https://github.com/brazil-data-cube/wlts for more information on WLTS.
+    """
     def __init__(self):
         pass
 
