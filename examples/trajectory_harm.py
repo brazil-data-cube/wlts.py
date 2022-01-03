eb802a5826666b003b326be531b4ec9e50cc2dff
Fabiana Zioti <fabiana_zioti@hotmail.com>
2022-01-03 16:07:08 -0300

    updating dates

--- examples/trajectory_harm.py
+++ examples/trajectory_harm.py
@@ -1,6 +1,6 @@
 #
 # This file is part of Python Client Library for WLTS.
-# Copyright (C) 2020-2021 INPE.
+# Copyright (C) 2020-2022 INPE.
 #
 # Python Client Library for WLTS is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
